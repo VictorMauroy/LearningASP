9e23a1016084a0fa382cb8a8e30f4b6c43c1e2df
VictorMauroy <mauroy.victor@gmail.com>
2023-06-29 15:46:06 +0200

    feat: :construction: :hammer: creation of a few get methods inside a controller, multiple views and a model.

---
+++ LearningASP/Controllers/Chapter.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace LearningASP.Controllers
+{
+    public class Chapter : Controller
+    {
+        // 
+        // GET: /Chapter/
+        public IActionResult Index()
+        {
+            return View();
+            //return "This is my default action...";
+        }
+
+        // 
+        // GET: /Chapter/Welcome?name=YourName&rank=YourRank
+        public IActionResult Welcome(string name, string rank)
+        {
+            /*   return HtmlEncoder.Default.Encode(
+                   $"Hello {name}, dear {rank}"
+                );
+            */
+
+            @ViewData["Name"] = HtmlEncoder.Default.Encode(name);
+            @ViewData["Rank"] = HtmlEncoder.Default.Encode(rank);
+            return View(); //Need to specify a View with the same name as the function
+        }
+
+        // 
+        // GET: /Chapter/Welcome/6?name=YourName
+        // Where '6' is the id added as an argument
+        public string WelcomeWithId(string name, int id)
+        {
+            return HtmlEncoder.Default.Encode(
+                $"Hello {name}, your user id is: {id}"
+            );
+        }
+    }
+}
---
+++ LearningASP/Models/Chapter.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LearningASP.Models
+{
+    public class Chapter
+    {
+        public int Id { get; set; }
+
+        public string? Title { get; set; }
+        
+        public string? Description { get; set; }
+        
+        [DataType(DataType.Date)]
+        public DateTime? CreatedDate { get; set; } = default;
+        
+        public string? Author { get; set; }
+        
+    }
+}
